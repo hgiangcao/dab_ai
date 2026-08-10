dd7719782f97566dbdf5be7cc35ba5bf616461dc
MCUT PC1 <you@example.com>
2026-08-10 16:36:21 +0000

    fix bug

--- distributed/selfplay.py
+++ distributed/selfplay.py
@@ -205,9 +205,7 @@ class SelfPlayGenerator:
         
 
         print ("Phase:", current_phase, "winrate", phase_winrate)
-        
-        import json
-        bot_stats_json = json.dumps(bot_stats)
+                bot_stats_json = json.dumps(bot_stats)
 
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         np.savez_compressed(
